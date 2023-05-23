9d7dbad47bdb0e60f7749185c6b5ec81488d443f
E Prasetyo <tarsisiusekoprasetyo11@gmail.com>
2023-05-23 21:15:21 +0700

    pages partner

--- src/views/dashboard/partners/index.jsx
+++ src/views/dashboard/partners/index.jsx
@@ -1,7 +1,26 @@
+import Table from './Table'
 import React from "react";
 
 const Partners = () => {
-  return <div className="mt-3">Partners</div>;
+  return (
+    <div className="mt-3">
+      <div className='p-4 flex flex-row justify-end items-center'>
+        <button 
+          className='
+            py-2 px-8 text-white font-bold bg-blue-600 text-center rounded-full
+            hover:bg-blue-900
+          '
+        >
+          ADD
+        </button>
+      </div>
+      <div className='flex flex-col bg-white'>
+        <div>
+          <Table />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Partners;
