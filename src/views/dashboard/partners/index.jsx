879fa33a3661c4f6b7ee67669e1586e20087e0fa
E Prasetyo <tarsisiusekoprasetyo11@gmail.com>
2023-05-31 14:47:41 +0700

    update page-partner

--- src/views/dashboard/partners/index.jsx
+++ src/views/dashboard/partners/index.jsx
@@ -1,24 +1,35 @@
-import Table from './Table'
+import Table from './component/PartnerTable'
 import React from "react";
+import partnerData from './partners.json'
 
 const Partners = () => {
+
+  const columnDataUser = [
+    {
+      Header: "IMAGE",
+      accessor: "image",
+    },
+    {
+      Header: "TITLE",
+      accessor: "title",
+    },
+    {
+      Header: "LINK",
+      accessor: "link",
+    },
+    {
+      Header: "EDIT",
+      accessor: "",
+    },
+    {
+      Header: "DELETE",
+      accessor: "",
+    },
+  ];
+
   return (
-    <div className="mt-3">
-      <div className='p-4 flex flex-row justify-end items-center'>
-        <button 
-          className='
-            py-2 px-8 text-white font-bold bg-blue-600 text-center rounded-full
-            hover:bg-blue-900
-          '
-        >
-          ADD
-        </button>
-      </div>
-      <div className='flex flex-col bg-white'>
-        <div>
-          <Table />
-        </div>
-      </div>
+    <div className="mt-8 bg-white rounded-full">
+      <Table columnsData={columnDataUser} tableData={partnerData} />
     </div>
   );
 };
