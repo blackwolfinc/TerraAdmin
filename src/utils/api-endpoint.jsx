e2214dd235cd9725bbbe1db0b3aa28fb20577f73
muafalah <muh.ifa.amrillah@gmail.com>
2023-06-12 02:01:47 +0700

    delete product & user dummy json

--- src/utils/api-endpoint.jsx
+++ src/utils/api-endpoint.jsx
@@ -34,6 +34,4 @@ export const API_ENDPOINT = {
   PATCH_PROMO: "/promo",
   DELETE_PROMO: "/promo",
   POST_IMAGE_PROMO: "/promo/image/upload/",
-  //CUSTOMERS
-  GET_ALL_CUSTOMERS: "/customer/",
 };
